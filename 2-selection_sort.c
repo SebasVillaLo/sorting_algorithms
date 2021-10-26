516afd347dc48461da654c04fe5322bd60197a8b
SebasVillaLo <villalopezsebastian@gmail.com>
2021-10-26 16:27:32 -0500

    add 2

---
+++ 2-selection_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+/**
+ * selections_sort - Sort
+ * @array: int
+ * @size: Size_t
+ * Return: Always 0
+ */
+void selections_sort(int *array, size_t size)
+{
+	size_t i, j, min_idx, aux;
+
+	if (size < 2 || array == NULL)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_idx = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_idx])
+				min_idx = j;
+		}
+		if (min_idx != i)
+		{
+			aux = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = aux;
+			print_array(array, size);
+		}
+	}
+}
