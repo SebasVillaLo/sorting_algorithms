df158de2655c245fc14b6e208c115b6f5decb423
SebasVillaLo <villalopezsebastian@gmail.com>
2021-10-26 13:02:46 -0500

    add 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,18 +9,21 @@
 
 void bubble_sort(int *array, size_t size)
 {
-    size_t aux, i, j;
+	size_t aux, i, j;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        for (j = 0; j < size - i - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                aux = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = aux;
-            }
-        }
-    }
+	if (size < 2 || array == NULL)
+		return;
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				aux = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = aux;
+				print_array(array, size);
+			}
+		}
+	}
 }
